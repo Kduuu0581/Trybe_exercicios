c0296a37297c7abfd28c66fb087898db6344764a
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2022-06-22 18:34:28 -0300

    exercicio 1 concluido!

---
+++ Fundamentos/4 - Introdução a JavaScript e Lógica de Programação/dia-1-javascript-primeiros-passos/exercicio_8.js
@@ -0,0 +1,13 @@
+
+const a = 1;
+const b = 3;
+const c = 5;
+
+let ePar = false;
+
+if ((a % 2 === 0 || b % 2 === 0 || c % 2 === 0)) {
+  ePar = true;
+};
+console.log(ePar);
+
+// importante para o projeto
