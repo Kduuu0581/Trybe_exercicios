5a63cb9fb0a0f733f61e37791a70c442e770673d
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2022-06-22 11:29:23 -0300

    exercicio 3 concluido!

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio_3.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio_3.js
@@ -3,7 +3,9 @@ let b = 4;
 let c = 2;
 
 if (a > b && a > c) {
-  console.log("Maior");
-} else if (b > c) {
-  console.log("Maior");
+  console.log("O maior número é: " + a + " (a)");
+} else if (b > a && b > c) {
+  console.log("O maior número é: " + b + " (b)");
+} else {
+  console.log("O maior número é: " + c + " (c)");
 }
