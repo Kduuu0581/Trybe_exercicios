6430dbc8fd386861c2923077be7ab6facb8cb4e9
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2022-07-15 13:29:57 -0300

    exercicio 3 parte 2 feito

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -40,3 +40,4 @@ console.log(
 const longestWord = (text) => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0];
 
 console.log(longestWord('Antonio foi ao banheiro e não sabemos o que aconteceu'));
+
