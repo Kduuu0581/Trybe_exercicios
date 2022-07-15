e2c2bc4df7f15b9104fc4378722d7144f7a3476f
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2022-07-15 13:57:09 -0300

    exercicio 4 parte 2 feito

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -41,3 +41,27 @@ const longestWord = (text) => text.split(' ').sort((wordA, wordB) => wordB.lengt
 
 console.log(longestWord('Antonio foi ao banheiro e não sabemos o que aconteceu'));
 
+const substituaX = (nome) => {
+  const frase = 'Tryber x aqui!';
+  const fraseArray = frase.split(' ');
+  for (let index = 0; index < fraseArray.length; index += 1) {
+if (fraseArray[index] === 'x') {
+  fraseArray[index] = nome;
+}
+  }
+  return fraseArray.join(' ');
+};
+console.log(substituaX('Romario'));
+
+const minhasSkills = (func) => {
+const skills = ['JavaScript', 'HTML', 'CSS'];
+let resultado = {
+  ${func}
+  `Minhas três principais habilidades são:`;
+  skills.forEach((skill) => {
+    resultado = `${resultado}
+    - ${skill}`;
+}); // reatribui e adiciona a skill atual a variável resultado
+return resultado;
+};
+console.log(minhasSkills(substituaX('Romario')));
